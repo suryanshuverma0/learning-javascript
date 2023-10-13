f39acfc5cf42201ce179962ca41aa07b223584e9
suryanshu0-coder <110730590+suryanshu0-coder@users.noreply.github.com>
2023-10-13 21:18:09 +0545

    adding string concepts

--- baic js/20-string.js
+++ baic js/20-string.js
@@ -21,3 +21,33 @@ console.log(details.includes("Name"));
 /*
 Note: We cannot change our original strings using these methods, these methods only makes a copy of that string.
 */
+
+console.log(String.fromCodePoint(90));
+let alphabet= "";
+for(let i = 65 ; i <=220 ; i++){
+         alphabet= alphabet+ String.fromCodePoint(i);
+}
+console.log(alphabet);
+
+let str1= "Super station and the process";
+let str2= "Andrew infinite or controlled looping and disappear";
+
+console.log(str1.localeCompare(str2));// returns 1 (integer)
+
+
+let s1 = "Looping";
+let s2 = "Looping";
+console.log(s1.localeCompare(s2));
+
+let st1 = "looping";
+let st2 = "Looping";
+console.log(st1.localeCompare(st2)); // returns -1
+
+
+/*
+The call str.localeCompare(str2) returns an integer indicating whether str is less, equal or greater than str2 according to the language rules:
+
+Returns a negative number if str is less than str2.
+Returns a positive number if str is greater than str2.
+Returns 0 if they are equivalent.
+*/
--- baic js/41_NaN.js
+++ baic js/41_NaN.js
@@ -1,4 +1,4 @@
-console.log(isNaN("str"));
+// console.log(isNaN("str"));
 // console.log(isNaN(123));
 // console.log(isNaN(NaN));
 // console.log(isNaN(true));
@@ -14,3 +14,15 @@ console.log(isNaN("str"));
 // console.log(isNaN({ a: 1, b: 2 }));
 // console.log(isNaN({ a: 1, b: 2 }));
 // console.log(isNaN(function () { }));
+
+// NaN is a special value in javascript which is not equal to itself
+//console.log(NaN == NaN);
+//console.log(NaN === NaN);
+
+console.log(Number.isNaN(123));
+console.log(isNaN("str"));
+console.log(isFinite(55));
+console.log(isFinite("555"));
+console.log(isFinite("str"));
+
+//Number.isNaN() and Number.isFinite() is more reliable than that of the isNaN and isFinite 
