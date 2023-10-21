a09917b95feb7eeed7945e8c15261db1168f7feb
suryanshu0-coder <110730590+suryanshu0-coder@users.noreply.github.com>
2023-10-21 16:08:07 +0545

    recursion

---
+++ baic js/49_recursion.js
@@ -0,0 +1,51 @@
+const company = {
+  sales: [
+    {
+      name: "John",
+      salary: 10,
+    },
+    {
+      name: "Roman",
+      salary: 18,
+    },
+  ],
+
+  development: {
+    sites: [
+      {
+        name: "Random",
+        salary: 30,
+      },
+      {
+        name: "Ronaldo",
+        salary: 50,
+      },
+    ],
+    internal: [
+      {
+        name: "Ram",
+        salary: 75,
+      },
+      {
+        name: "Bravo",
+        salary: 5,
+      },
+    ],
+  },
+};
+
+function calculateSalary(department) {
+  if (Array.isArray(department)) {
+    return department.reduce((previous, current) => {
+      return previous + current.salary;
+    }, 0);
+  } else {
+    let sum = 0;
+    for (let sub of Object.values(department)) {
+      sum = sum + calculateSalary(sub);
+    }
+    return sum;
+  }
+}
+
+console.log(calculateSalary(company));
