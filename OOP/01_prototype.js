bdf454a0143a70d0c8807b15c016207a07249d2f
suryanshu0-coder <110730590+suryanshu0-coder@users.noreply.github.com>
2023-10-23 22:47:00 +0545

    learning oop

---
+++ OOP/01_prototype.js
@@ -0,0 +1,27 @@
+//creating a object
+
+let a ={
+         name: "Suryanshu Verma",
+         age: 19,
+         sayHi : ()=>{
+                  console.log(`Hi , ${this.name}`)
+         }
+}
+
+p ={
+         programming : true,
+         engineering : true,
+}
+
+a.__proto__ =p;
+console.log(a.programming, a.engineering);  //first it searches in object a  and if not found it searches in object p theis is prototype
+
+p.__proto__ ={
+         color:"red",
+         field: "Information Technology",
+}
+
+console.log(a.color);// first it searches in object a and if not found it searches in object p and if not found it searches in object prototype of the object  this chain is continous 
+//we can create as many prototype chain like this.
+
+//It is null or reference of object 
