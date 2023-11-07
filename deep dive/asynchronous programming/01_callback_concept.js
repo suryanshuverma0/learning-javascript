a9d20f548a3f82257ac3b79266e90e77241b1dd6
suryanshu0-coder <110730590+suryanshu0-coder@users.noreply.github.com>
2023-11-07 23:32:48 +0545

    learining asynchronous programming basics

---
+++ deep dive/asynchronous programming/01_callback_concept.js
@@ -0,0 +1,15 @@
+function myDisplayer(some){
+         console.log(some);
+
+}
+
+function furstFunction(){
+         myDisplayer("Hi");
+}
+
+function secondFunction(){
+         myDisplayer("Goodbye");
+}
+
+secondFunction();
+furstFunction();
